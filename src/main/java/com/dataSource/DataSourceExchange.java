fdbb110db8af3cef08764681667293ddf4db6708
siling <2606726832@qq.com>
2016-10-20 19:17:20 +0800

    动态配置文件+默认数据库源dev

--- src/main/java/com/dataSource/DataSourceExchange.java
+++ src/main/java/com/dataSource/DataSourceExchange.java
@@ -4,24 +4,27 @@ import java.lang.reflect.Method;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
-import org.springframework.core.annotation.AnnotationUtils;
 
 public class DataSourceExchange implements MethodInterceptor{
 
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
-       String methodName = invocation.getMethod().getName();
-       Method methodObj = null;
-       Method[] methods = invocation.getThis().getClass().getMethods();
-       for(Method method : methods) {
-           if(method.getName().equals(methodName)) {
-               methodObj = method;
-               break;
-           }
-       }
-       
-        DataSource dataSource = AnnotationUtils.findAnnotation(methodObj, DataSource.class);
-        DataSourceHolder.setDatasources(dataSource.value());
+        String methodName = invocation.getMethod().getName();
+        Method methodObj = null;
+        Method[] methods = invocation.getThis().getClass().getMethods();
+        for (Method method : methods) {
+            if (method.getName().equals(methodName)) {
+                methodObj = method;
+                break;
+            }
+        }
+
+        if(methodObj.isAnnotationPresent(DataSource.class)) {
+            DataSource dataSource = methodObj.getAnnotation(DataSource.class);
+            DataSourceHolder.setDatasources(dataSource.value());
+        } else {
+            DataSourceHolder.setDatasources(DataSource.master); // 默认master
+        }
         return invocation.proceed();
     }
 
--- src/main/java/com/service/impl/UserServiceImpl.java
+++ src/main/java/com/service/impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.dao.UserDao;
 import com.dataSource.DataSource;
@@ -22,7 +21,7 @@ public class UserServiceImpl implements UserService {
     }
     
     @Override
-    @DataSource(value = DataSource.bk)
+    @DataSource(value = DataSource.bk) // 不加@DataSource就默认master
     public List<String> testDataSourceBk() {
         return userDao.queryAllName();
     }
