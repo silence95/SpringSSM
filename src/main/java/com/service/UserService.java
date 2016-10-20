30700437a2e949b987c731650e5f34b743c4e5e3
siling <2606726832@qq.com>
2016-10-20 20:18:14 +0800

    springaop内部方法能否生效test

--- src/main/java/com/service/UserService.java
+++ src/main/java/com/service/UserService.java
@@ -2,12 +2,12 @@ package com.service;
 
 import java.util.List;
 
-import com.dataSource.DataSource;
-
 public interface UserService {
    
-    @DataSource
     public List<String> queryUser();
     
     public List<String> testDataSourceBk();
+    
+    public List<String> testSpringBean();
+    
 }
--- src/main/java/com/service/impl/UserServiceImpl.java
+++ src/main/java/com/service/impl/UserServiceImpl.java
@@ -17,6 +17,9 @@ public class UserServiceImpl implements UserService {
     @Override
     @DataSource(value = DataSource.master)
     public List<String> queryUser() {
+        // return testSpringBean(); // 拦截不了，springAop的通知只能作用于springbean上，this调用自己的方法无效，可使用
+        // ((UserService)AopContext.currentProxy()).testSpringBean();
+        // 会抛错，添加<aop:config expose-proxy="true"></aop:config>配置
         return userDao.queryAllName();
     }
     
@@ -24,6 +27,15 @@ public class UserServiceImpl implements UserService {
     @DataSource(value = DataSource.bk) // 不加@DataSource就默认master
     public List<String> testDataSourceBk() {
         return userDao.queryAllName();
+    }
+    
+    @DataSource(value = DataSource.bk)
+    public List<String> testSpringBean() {
+        return userDao.queryAllName();
+    }
+    
+    public static void testStatic() {
+        
     }
 
 }
