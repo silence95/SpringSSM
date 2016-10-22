7b9c958c8bfe913b89140c6eb14d591f9aa7df21
siling <2606726832@qq.com>
2016-10-22 18:19:06 +0800

    aspectj dev

---
+++ src/main/java/com/aspect/TestAspect.java
@@ -0,0 +1,32 @@
+package com.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class TestAspect {
+
+    // 1、切点符合运算定义切面
+    @Before("execution(* queryUser(..))")
+    public void greetToRun() {
+        System.out.println("hello greetToRun running");
+    }
+    
+    // 2、命名切点
+    @Pointcut("execution(* queryUser(..))") private void beforeGreet() {}
+    
+    @Before("beforeGreet()")
+    public void beforeGreetToRun() {
+        System.out.println("hello beforeGreetToRun running");
+    }
+    
+    // 访问连接点信息
+    @Around("beforeGreet()")
+    public Object joinPointAccess(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("joinPointAccess access method is:" + pjp.getTarget().getClass());
+        return pjp.proceed();
+    }
+}
