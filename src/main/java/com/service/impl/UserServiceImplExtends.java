2e6e930a1efac0e49746ddacd60510f7a824dba2
siling <2606726832@qq.com>
2016-10-22 16:09:03 +0800

    add all

---
+++ src/main/java/com/service/impl/UserServiceImplExtends.java
@@ -0,0 +1,8 @@
+package com.service.impl;
+
+public class UserServiceImplExtends extends UserServiceImpl{
+
+    public static void main(String[] args) {
+        UserServiceImplExtends.testStatic();
+    }
+}
